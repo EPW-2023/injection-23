5e610448617d8b044b1075f25537eacc542e04b4
WhiteCat20 <fzrahmadan@gmail.com>
2022-11-01 18:22:49 +0700

    fixing coming soon and 404 page

--- resources/views/errors/comingsoon.blade.php
+++ resources/views/errors/comingsoon.blade.php
@@ -4,13 +4,13 @@
     <div id="bg-404-error">
         <div class="prev-404-construct">
             <div class="container">
-                <div class="wrapper-image-text">
+                <div class="wrapper-comingsoon-text">
                     <div class="">
-                        <img class="img-fluid" src="{{ asset('img/City.png') }}" alt="Future City in reality">
+                        <img class="img-fluid" src="{{ asset('img/City.png') }}" alt="Future City in reality" width="300"
+                            height="300">
                     </div>
-                    <div class="">
-                        <h1 class="on-404-text">404 Page Not Found</h1>
-                        <p class="on-404-paraf">Oops... something is wrong</p>
+                    <div>
+                        <h1 class="on-comingsoon-text fw-bold">COMING SOON</h1>
                     </div>
                 </div>
             </div>
