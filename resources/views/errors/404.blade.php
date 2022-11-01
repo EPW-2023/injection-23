890dcf36f688ef660caecc094bbde1f08b4f5dcf
WhiteCat20 <fzrahmadan@gmail.com>
2022-11-01 12:49:50 +0700

    adding 404 page not found to be merged

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.layout')
+@section('content')
+    <div id="bg-404-error">
+        <div class="prev-404-construct">
+            <div class="container">
+                <div class="wrapper-image-text">
+                    <div class="">
+                        <img class="img-fluid" src="{{ asset('img/City.png') }}" alt="Future City in reality">
+                    </div>
+                    <div class="">
+                        <h1 class="on-404-text">404 Page Not Found</h1>
+                        <p class="on-404-paraf">Oops... something is wrong</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+@endsection
