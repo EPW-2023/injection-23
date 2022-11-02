54a739d68ef5ca26502dfe09510653c3dc057712
Faiz Rahmadani <fzrahmadan@gmail.com>
2022-11-02 10:44:14 +0700

    trying fixing comingsoon and 404

--- resources/views/errors/comingsoon.blade.php
+++ resources/views/errors/comingsoon.blade.php
@@ -3,16 +3,19 @@
     <img src="{{ asset('img/Logo EPW.png') }}" class="logo-epw m-3" alt="Engineering Physics Week (EPW) Logo">
     <div id="bg-404-error">
         <div class="container-batik"><img src="{{ asset('img/Batik.png') }}" class="batik" alt=""></div>
-        <div class="prev-404-construct">
+        <div class="prev-comingsoon-construct">
             <div class="container">
-                <div class="wrapper-comingsoon-text">
-                    <div class="">
-                        <img class="img-fluid" src="{{ asset('img/City.png') }}" alt="Future City in reality" width="300"
-                            height="300">
+                <div>
+                    <div class="wrapper-comingsoon-text">
+                        <div>
+                            <img class="img-fluid" src="{{ asset('img/City.png') }}" alt="Future City in reality"
+                                width="300" height="300">
+                        </div>
+                        <div>
+                            <h1 class="on-comingsoon-text fw-bold">COMING SOON</h1>
+                        </div>
                     </div>
-                    <div>
-                        <h1 class="on-comingsoon-text fw-bold">COMING SOON</h1>
-                    </div>
+
                 </div>
             </div>
         </div>
