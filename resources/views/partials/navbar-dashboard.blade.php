c27bf90513bd5bd69bbf7d8b6b34bb730ad58a75
Haznul24 <abrarhaznul@gmail.com>
2023-02-08 17:01:12 +0700

    change navbar

--- resources/views/partials/navbar-dashboard.blade.php
+++ resources/views/partials/navbar-dashboard.blade.php
@@ -1,42 +1,31 @@
-<nav class="navbar navbar-epw navbar-expand-lg px-5">
+<nav class="navbar navbar-epw navbar-expand-lg bg-transparent">
     <div class="container-fluid">
         <img src="{{ asset('img/Logo EPW.png') }}" width="60" alt="">
         <a class="navbar-brand ms-4" href="#">EPW 2023</a>
-        <button type="button" class="btn btn-primary bg-transparent border-0" id="button-modal" data-bs-toggle="modal"
-            data-bs-target="#exampleModal">
-            <i class="bi bi-list fs-3"></i>
+        <button type="button" class="btn btn-primary  bg-transparent border-0" id="button-modal" type="button"
+            data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fa-solid fa-bars fs-3"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 @guest()
-                    <li class="nav-item">
-                        <a class="nav-link nav-link-epw" id="nav-link-epw" href="#">LOGIN</a>
+                    <li class="nav-item text-center">
+                        <a class="nav-link nav-link-epw" id="nav-link-epw" href="/login">Login</a>
                     </li>
-                @endguest
-                @auth()
-                    <li class="nav-item">
-                        <form action="{{ route('applicant-logout') }}" method="post">
-                            @csrf
-                            <button class="nav-link nav-link-epw bg-transparent applicant-logout-button"
-                                id="nav-link-epw">LOGOUT</button>
-                        </form>
-                    </li>
-                @endauth
+                    @endguest
+                    @auth()
+                        <li class="nav-item">
+                            <form action="{{ route('applicant-logout') }}" method="POST">
+                                @csrf
+                                <li class="nav-item text-center">
+                                    <a class="nav-link nav-link-epw" id="nav-link-epw" href="/login">Logout</a>
+                                </li>
+                            </form>
+                        </li>
+                    @endauth
             </ul>
         </div>
     </div>
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">EPW 2023</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body text-center">
-                    <a href="#" class="d-block text-decoration-none my-2 link-modal">Logout</a>
-                </div>
-            </div>
-        </div>
-    </div>
+
 </nav>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,31 +1,18 @@
-<nav class="navbar navbar-epw navbar-expand-lg px-5">
+<nav class="navbar navbar-epw navbar-expand-lg">
     <div class="container-fluid">
         <img src="{{ asset('img/Logo EPW.png') }}" width="60" alt="">
         <a class="navbar-brand ms-4" href="#">EPW 2023</a>
-        <button type="button" class="btn btn-primary bg-transparent border-0" id="button-modal" data-bs-toggle="modal"
-            data-bs-target="#exampleModal">
-            <i class="bi bi-list fs-3"></i>
+        <button type="button" class="btn btn-primary  bg-transparent border-0" id="button-modal" type="button"
+            data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fa-solid fa-bars fs-3"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link nav-link-epw" id="nav-link-epw" href="{{ route('login') }}">LOGIN</a>
+                <li class="nav-item text-center">
+                    <a class="nav-link nav-link-epw" id="nav-link-epw" href="/login">Login</a>
                 </li>
             </ul>
-        </div>
-    </div>
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">EPW 2023</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body text-center">
-                    <a href="{{ route('login') }}" class="d-block text-decoration-none my-2 link-modal">LOGIN</a>
-                </div>
-            </div>
         </div>
     </div>
 </nav>
